ec21502e865c6481e288e5e389d14381cfbc9778
Sungjae Park <park1637@morris.umn.edu>
2018-02-08 14:45:48 -0600

    Copied methods from the user database to the todo database so that it can return all todos

--- src/main/java/umm3601/todo/Database.java
+++ src/main/java/umm3601/todo/Database.java
@@ -1,4 +1,20 @@
 package umm3601.todo;
 
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Database {
+  private Todo[] allTodos;
+
+  public Database(String userDataFile) throws IOException {
+    Gson gson = new Gson();
+    FileReader reader = new FileReader(userDataFile);
+    allTodos = gson.fromJson(reader, Todo[].class);
+  }
+
+  public Todo[] listTodos() {
+    return allTodos;
+  }
 }
