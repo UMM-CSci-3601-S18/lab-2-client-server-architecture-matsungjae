d30051639c5ace7fc49d81c3431038a55d452815
Sungjae Park <park1637@morris.umn.edu>
2018-02-08 15:03:22 -0600

    updated both tests in FullTodoListFromDB

--- src/test/java/umm3601/todo/FullTodoListFromDB.java
+++ src/test/java/umm3601/todo/FullTodoListFromDB.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests umm3601.todo.Database listUser functionality
@@ -15,15 +16,26 @@ public class FullTodoListFromDB {
   @Test
   public void totalTodoCount() throws IOException {
     Database db = new Database("src/main/data/todos.json");
-    Todo[] allUsers = db.listTodos();
-    assertEquals("Incorrect total number of todos", 10, allUsers.length);
+    Todo[] allTodos = db.listTodos();
+    assertEquals("Incorrect total number of todos", 300, allTodos.length);
   }
 
   @Test
   public void firstUserInFullList() throws IOException {
-    Database db = new Database("src/main/data/users.json");
+    Database db = new Database("src/main/data/todos.json");
     Todo[] allTodos = db.listTodos();
     Todo firstTodo = allTodos[0];
     //TODO: tests for firstTodo
+    assertEquals( "Incorrect ID","58895985a22c04e761776d54", firstTodo._id);
+    assertEquals( "Incorrect Owner","Blanche", firstTodo.owner);
+    assertEquals( "Incorrect Status",false, firstTodo.status);
+    assertEquals( "Incorrect body","In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.", firstTodo.body);
+    assertEquals( "Incorrect category","software design", firstTodo.category);
+
+    /*"_id": "58895985a22c04e761776d54",
+      "owner": "Blanche",
+      "status": false,
+      "body": "In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.",
+      "category": "software design"*/
   }
 }
